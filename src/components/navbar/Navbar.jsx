8f7423640d7e858e4ec57ea8d9d23aaafb6a9bd8
paulinaweb <124458081+paulinaweb@users.noreply.github.com>
2024-06-29 14:18:12 +0200

    updated navbar text

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
     <>
       <nav className="navbar">
         <Link to="/" className="navbar-logo">
-          NATURE
+          Musiklektioner
           <Icons.FaTree />
         </Link>
         <ul className="nav-items">
