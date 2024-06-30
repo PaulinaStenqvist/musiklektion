7f04a5f86f907fbdd8707a3794b98ed5e3ff9cbf
paulinaweb <124458081+paulinaweb@users.noreply.github.com>
2024-06-30 12:44:03 +0200

    updated upload

--- src/pages/Document.jsx
+++ src/pages/Document.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Upload from "../components/upload/Upload";
 
 function Document() {
   return (
     <div>
       <h1 className="document">Dokument</h1>
+      < Upload />
     </div>
   );
 }
--- src/pages/Uppgifter.jsx
+++ src/pages/Uppgifter.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Upload from "../components/upload/Upload";
+
 
 function Uppgifter() {
   return (
     <div>
       <h1 className="marketing">Uppgifter</h1>
+      < Upload />
     </div>
   );
 }
